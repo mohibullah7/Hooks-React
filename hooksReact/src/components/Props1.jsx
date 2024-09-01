406c33fec09a51571e04caa8ab49ee132cae68c6
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-09-01 21:55:50 +0500

    more on props

--- hooksReact/src/components/Props1.jsx
+++ hooksReact/src/components/Props1.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Props1({ chacha }) {
-  const  { name, job, image } =chacha;
+function Props1({ chacha,index,chnageOnclick }) {
+  const  { name, job, image,friend } =chacha;
   return (
     <>
       <div className="w-48 h-62 bg-zinc-100 rounded-md  flex flex-col  overflow-hidden">
@@ -15,8 +15,9 @@ function Props1({ chacha }) {
         <div className="">
           <h1 className="text-sm font-bold px-3 mt-2">{name}</h1>
           <h2 className="text-xs px-3 font-semibold">{job}</h2>
-          <button className="px-4 py-[2px]  ms-3 mt-2 mb-2 bg-blue-500 rounded-md text-white text-sm">
-            Add friend
+          <button onClick={()=>chnageOnclick(index)}
+           className="px-4 py-[2px]  ms-3 mt-2 mb-2 bg-blue-500 rounded-md text-white text-sm">{friend === true ? 'Friend':"Add friend"}
+            
           </button>
         </div>
       </div>
--- hooksReact/src/components/PropsKaBaap.jsx
+++ hooksReact/src/components/PropsKaBaap.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Props1 from './Props1'
 
 function PropsKaBaap() {
@@ -6,27 +6,45 @@ function PropsKaBaap() {
     {
       name :'Mohib khan',
       job:'Programmer',
-      image:'https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      image:'https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      friend: false 
 
     },
     {
       name :'Rahul Gandhi',
       job:'Designer ',
-      image:'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      image:'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      friend: false 
+
 
     },
     {
       name :'Zardari',
       job:'Plumber',
-      image:'https://images.pexels.com/photos/4505269/pexels-photo-4505269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      image:'https://images.pexels.com/photos/4505269/pexels-photo-4505269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      friend: false 
+
 
     }
   ]
+  const [values,Setvalues]=useState(data)
+
+  const chnageOnclick=(changeindex)=>{
+    
+  
+    Setvalues((prev)=>{
+      return prev.map((item,index)=>{
+        if (index === changeindex) return{...item,friend: !item.friend};
+          return item;
+        
+      })
+    })
+  }
   return (
     <div>
       <div className="w-full h-screen bg-zinc-300 flex justify-center items-center gap-10">
-        {data.map((item,index)=>(
-      <Props1 chacha={item}  ></Props1>
+        {values.map((item,index)=>(
+      <Props1 chacha={item} index={index} chnageOnclick={chnageOnclick} ></Props1>
 
         ))}
 
